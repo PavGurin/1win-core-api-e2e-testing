81cc3c21ebc80d2c41bd34cc6cfa3ffb75089952
akostah <akostah@1win.pro>
2019-01-23 14:52:24 +0300

    add test cases to casino search

--- test/test_casino/casino_search.js
+++ test/test_casino/casino_search.js
@@ -4,14 +4,51 @@ import { expect } from "chai"
 
 describe("///////////////////casino search check ///////////////////////////", () => {
 
-    it("////////////////////bets make ordinar//////////////////", async () => {
-        const { data } = socket.send("CASINO:games-search",{
+    it("////////////////////games-search//////////////////", async () => {
+        const { data } = await socket.send("CASINO-2:games-search",{
 
         })
-       
-        console.log(data);
-     // expect(data.message).to.equal(undefined);
+
+       //console.log(data[0]);
+
+        expect(data).to.be.an('array');
+        expect(data[0].name).to.be.an('object');
+        expect(data.message).to.equal(undefined);
+        
     })
+
+
+
+    it("////////////////////categories-all/////////////////", async () => {
+        const { data } = await socket.send("CASINO-2:categories-all",{
+
+
+        });
+   
+        //console.log(data[0]);
+        expect(data).to.be.an('array');
+        expect(data[0].Name).to.be.an('object');
+        expect(data.message).to.equal(undefined);
+    })
+
+
+    it("////////////////////games-all//////////////////", async () => {
+        const { data } =   await socket.send("CASINO-2:games-all",{
+            limit: [0,1000],
+            where: {
+           
+            
+            },
+            lang: "ru",
+            isOnlyMobile: false
+          })
+        //console.log(data[0]);
+        expect(data).to.be.an('array');
+        expect(data[0].name).to.be.an('object');
+        expect(data.message).to.equal(undefined);
+    })
+
+   
 
 
 })
