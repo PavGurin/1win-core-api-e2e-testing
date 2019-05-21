9c5e26bf8959d4ddb6a080b325e6ffc7248cae64
Lina <lina.qa88@gmail.com>
2019-05-21 18:20:53 +0300

    update tests for auth

--- test/test_profile_auth/micro_profile_auth_spec.js
+++ test/test_profile_auth/micro_profile_auth_spec.js
@@ -4,12 +4,12 @@ import { expect } from "chai"
 describe("Profile check click/mail", () => {
 
     it("Auth login from 1 click", async () => {
-      const { data } = await socket.send("PROFILE:auth-login",{login:'rgckho@1win.xyz',password:'55s9ef'});
+      const { data } = await socket.send("POST:login",{login:'rgckho@1win.xyz',password:'55s9ef'});
       expect(data.message).to.equal(undefined);
     })
 
     it("Auth login from mail", async () => {
-        const { data } = await socket.send("PROFILE:auth-login",{login:'testsocke777@yandex.ru',password:'qwerty777'});
+        const { data } = await socket.send("POST:login",{login:'testsocke777@yandex.ru',password:'qwerty777'});
         expect(data.message).to.equal(undefined);
     })
 
