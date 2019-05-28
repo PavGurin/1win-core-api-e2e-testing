6f93ae5733aa8d53d2dbec508529db138e6932b4
pavgurin <kalliak1993@yandex.ru>
2019-05-28 16:59:04 +0300

    [+] added return for userList methods to be able to debug responses

--- src/userList.js
+++ src/userList.js
@@ -2,16 +2,15 @@ export const userList = {
 
     //Prodlike
     async login_without_money() {
-        await socket.send('USER:auth-login', {login: '123123@mailinator.com', password: '123123'});
+        return socket.send('USER:auth-login', {login: '123123@mailinator.com', password: '123123'});
     },
 
     async login_with_RUB() {
-        await socket.send('USER:auth-login', {login: 'test_withdrawal@mailinator.com', password: '123123'});
+        return socket.send('USER:auth-login', {login: 'test_withdrawal@mailinator.com', password: '123123'});
     },
     async login_with_RUB_USD() {
-        await socket.send('USER:auth-login', {login: 'test_withdrawal2@mailinator.com', password: '123123'});
-    },
-
+        return socket.send('USER:auth-login', {login: 'test_withdrawal2@mailinator.com', password: '123123'});
+    }
     //Prod
-}
+};
 
