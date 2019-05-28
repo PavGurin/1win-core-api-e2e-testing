70edee3846f5ead1e9b7a71bfee3f00a4f40b213
pavgurin <kalliak1993@yandex.ru>
2019-05-28 16:51:48 +0300

    [+] promise waiter for balance requests

--- test/test-banking/getBalance.js
+++ test/test-banking/getBalance.js
@@ -4,21 +4,21 @@ import {userList} from '../../src/userList';
 describe('Balance get', () => {
 
     it('Without money', async () => {
-        userList.login_without_money();
+        await userList.login_without_money();
         const {data} = await socket.send('BANKING:balance-get');
         console.log(data);
         expect(data.message).equal(undefined);
     });
 
     it('With money only rub', async () => {
-        userList.login_with_RUB();
+        await userList.login_with_RUB();
         const {data} = await socket.send('BANKING:balance-get');
         console.log(data);
         expect(data.message).equal(undefined);
     });
 
     it('With money rub + usd', async () => {
-        userList.login_with_RUB();
+        await userList.login_with_RUB();
         const {data} = await socket.send('BANKING:balance-get');
         console.log(data);
         expect(data.message).equal(undefined);
