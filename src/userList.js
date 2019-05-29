9adc9edd0a8e4e1a08120e5bbea122e15f0a3e0b
Lina <lina.qa88@gmail.com>
2019-05-29 16:08:22 +0300

    [+] -  add new users with different status(tester, full block, partial block)

--- src/userList.js
+++ src/userList.js
@@ -12,7 +12,22 @@ export const userList = {
         //Должен быть баланс в валюте отличной от рублей
         await socket.send('USER:auth-login', {login: 'test_withdrawal2@mailinator.com', password: '123123'});
     },
+    async login_test_status() {
+        //У пользователя статит статус тестового пользователя
+        await socket.send('USER:auth-login', {login: 'tester_status@mailinator.com', password: '123123'});
+    },
+
+    async login_full_block() {
+        //У пользователя стоит полный блок
+        await socket.send('USER:auth-login', {login: 'full_block_user@mailinator.com', password: '123123'});
+    },
+
+    async login_partial_block() {
+        //У пользователя стоит частичный блок
+        await socket.send('USER:auth-login', {login: 'partial_block_user@mailinator.com', password: '123123'});
+    },
+
 
     //Prod
-}
+};
 
