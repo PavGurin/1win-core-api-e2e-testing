7bb9d503d3601faa8053e684e247aa613a5af37e
Lina <lina.qa88@gmail.com>
2019-05-29 16:22:25 +0300

    [+] -  add new tests on get Withdrawal on id

--- test/test-banking/withdrawal.js
+++ test/test-banking/withdrawal.js
@@ -25,11 +25,28 @@ describe('withdrawal', () => {
         expect(data).to.deep.include({message: 'Выплата не найдена'});
     });
 
-    it('Get - Withdrawal 404 not found - unauthorized ', async () => {
-
-        const {data} = await socket.send('BANKING:withdrawal-get', {id: 'c0ncvf4m'});
+    it('Get - Withdrawal 100 RUB money-transfer -  ', async () => {
+        await userList.login_with_RUB();
+        const {data} = await socket.send('BANKING:withdrawal-get', {id: 163});
         console.log(data);
         expect(data.message).equal(undefined);
+
+    });
+
+    it('Get - Withdrawal 1 RUB card_rub', async () => {
+        await userList.login_with_RUB();
+        const {data} = await socket.send('BANKING:withdrawal-get', {id: 179});
+        console.log(data);
+        expect(data.message).equal(undefined);
+
+    });
+
+    it('Get - Withdrawal 404 not found - unauthorized ', async () => {
+
+        const {data} = await socket.send('BANKING:withdrawal-get', {id: '162'});
+        console.log(data);
+        expect(data).to.deep.include({status: 404});
+        expect(data).to.deep.include({message: 'Выплата не найдена'});
     });
 
 });
