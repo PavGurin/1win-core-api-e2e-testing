b7bb9ed74052eca10aedc39f18ed44420a5d9de6
Lina <lina.qa88@gmail.com>
2019-05-30 22:59:40 +0300

    [~] fix message for 2 tests

--- test/test-banking/withdrawal_confirm.js
+++ test/test-banking/withdrawal_confirm.js
@@ -9,5 +9,6 @@ describe('Withdrawal confirm', () => {
         console.log(data);
         expect(data.status).equal(400);
         expect(data.message).equal('Неверный ключ запроса');
+
     });
 });
