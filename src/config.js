82040c822a70c97726975be28bd6b1532badc846
Lina <lina.qa88@gmail.com>
2019-05-31 17:55:09 +0300

    [~] - update config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,10 @@
 export default {
 
-    // prodLike
+    // backend
     backendURL: process.env.TEST_URL || 'https://backend.1win-prodlike.tech/'
+
+    // dev
+    // backendURL: process.env.TEST_URL || 'https://1win.dev/'
 
     // prod
     //backendURL: process.env.TEST_URL || 'https://1wnmh.xyz/'
