60555de12bab45c97af07947074a9de834af3ea6
Lina <lina.qa88@gmail.com>
2019-06-01 03:00:09 +0300

    [+] commented console.log

--- test/test-banking/getMaps.js
+++ test/test-banking/getMaps.js
@@ -6,7 +6,7 @@ describe('Get methods map', () => {
     it('C19355 Withdrawal methods map with login', async () => {
         await userList.login_without_money();
         const {data} = await socket.send('BANKING:methods-withdrawal');
-        console.log(data);
+        //console.log(data);
         expect(data).to.be.an('object');
         expect(data.card_rub.placeholder_text.en).equal('Bank card number');
         expect(data.card_rub.placeholder_text.ru).equal('Номер банковской карты');
@@ -15,7 +15,7 @@ describe('Get methods map', () => {
     it('C19356 Withdrawal methods map without login', async () => {
 
         const {data} = await socket.send('BANKING:methods-withdrawal');
-        console.log(data);
+        //console.log(data);
         expect(data).to.be.an('object');
         expect(data.card_rub.placeholder_text.en).equal('Bank card number');
         expect(data.card_rub.placeholder_text.ru).equal('Номер банковской карты');
@@ -24,7 +24,7 @@ describe('Get methods map', () => {
     it('C19357 Payment methods map with login', async () => {
         await userList.login_without_money();
         const {data} = await socket.send('BANKING:methods-payment');
-        console.log(data);
+        //console.log(data);
         expect(data).to.be.an('object');
         expect(data.card_rub.text.en).equal('Bank card');
         expect(data.card_rub.text.ru).equal('Банковская карта');
@@ -33,7 +33,7 @@ describe('Get methods map', () => {
     it('C19358 Payment methods map without login', async () => {
 
         const {data} = await socket.send('BANKING:methods-payment');
-        console.log(data);
+        //console.log(data);
         expect(data).to.be.an('object');
         expect(data.card_rub.text.en).equal('Bank card');
         expect(data.card_rub.text.ru).equal('Банковская карта');
