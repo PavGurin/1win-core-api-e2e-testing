85571acba67e6390de57b9491b9e7a4fad431108
Alex <alex-top@1win.xyz>
2019-06-11 17:51:41 +0300

    - Add testrail integration

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 export default {
 
     // backend
-    backendURL: process.env.TEST_URL || 'https://backend.1win-prodlike.tech/'
+    // backendURL: process.env.TEST_URL || 'https://backend.1win-prodlike.tech/'
 
     // dev
     // backendURL: process.env.TEST_URL || 'https://1win.dev/'
 
     // prod
-    //backendURL: process.env.TEST_URL || 'https://1wnmh.xyz/'
+    backendURL: process.env.TEST_URL || 'https://1winufeg.xyz/'
 }
