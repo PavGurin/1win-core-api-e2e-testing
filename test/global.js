8c1a8f3f122f5bbfac07da9a17c3db0978815d86
pavgurin <kalliak1993@yandex.ru>
2019-06-14 18:53:49 +0300

    [~] optimized format

--- test/global.js
+++ test/global.js
@@ -1,13 +1,13 @@
-import SocketClient from "../src"
+import SocketClient from '../src';
 
 let socket;
 
 before(async () => {
-  socket = new SocketClient({}) 
-  await socket.connect();
-  global.socket = socket
-})
+    socket = new SocketClient({});
+    await socket.connect();
+    global.socket = socket;
+});
 
 after(async () => {
     socket.disconnect();
-})
+});
