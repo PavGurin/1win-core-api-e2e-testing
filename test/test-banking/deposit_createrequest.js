5275f8364d7090522d578adecb4c09966e951226
Lina <lina.qa88@gmail.com>
2019-06-18 14:51:09 +0300

    [~] - add @master

--- test/test-banking/deposit_createrequest.js
+++ test/test-banking/deposit_createrequest.js
@@ -3,17 +3,18 @@ import {register} from "../../src/register";
 
 describe('Deposit requests', () => {
 
-    it('C19376 (-) create without currency', async () => {
+    it('C19376 (-) create without currency @master', async () => {
         await register.one_click_reg();
         const {data} = await socket.send('BANKING:deposit-create-request', {
             amount: 100,
             wallet: '',
             paymentType: 'card_rub'});
-        // console.log(data);
+        //console.log(data);
+        expect(data.redirectUrl).not.equal(null);
         expect(data.message).equal(undefined);
     });
 
-    it(' (+) create with currency', async () => {
+    it(' (+) create with currency @master', async () => {
         await register.one_click_reg();
         const {data} = await socket.send('BANKING:deposit-create-request', {
             amount: 100,
@@ -21,14 +22,15 @@ describe('Deposit requests', () => {
             paymentType: 'card_rub',
             currency: 'RUB'
         });
-        // console.log(data);
+        //console.log(data);
+        expect(data.redirectUrl).not.equal(null);
         expect(data.message).equal(undefined);
     });
 
     it('C19377 (-) with hash', async () => {
         await register.one_click_reg();
         const {data} = await socket.send('BANKING:deposit-request', {h: 'gjhg'});
-        // console.log(data);
+        console.log(data);
         expect(data.status).equal(404);
         expect(data.message).equal('Запрос депозита не найден');
     });
