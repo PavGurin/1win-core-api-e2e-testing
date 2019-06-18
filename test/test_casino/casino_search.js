7777d1db2da4220c57c13f701ddf3c048082a875
Lina <lina.qa88@gmail.com>
2019-06-18 12:33:02 +0300

    [~] - delete lable @master

--- test/test_casino/casino_search.js
+++ test/test_casino/casino_search.js
@@ -3,7 +3,7 @@ import {expect} from "chai"
 describe("Casino search check @master", () => {
 
     //TODO нужно разобраться почему 500 и завести задачу на бэк
-    it("C20487 - Games-search @master", async () => {
+    it("C20487 - Games-search", async () => {
         const { data } = await socket.send("CASINO-2:games-search",{
         });
 
@@ -12,7 +12,7 @@ describe("Casino search check @master", () => {
     });
 
 
-    it("C20488 Categories-all", async () => {
+    it("C20488 - Categories-all", async () => {
         const { data } = await socket.send("CASINO-2:categories-all",{
         });
         //console.log(data[0]);
