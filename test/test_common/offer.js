e9ef6297c23942fa600dd3cb91d1987f041267ee
Lina <lina.qa88@gmail.com>
2019-06-19 14:55:53 +0300

    [~] - fix expect

--- test/test_common/offer.js
+++ test/test_common/offer.js
@@ -7,21 +7,21 @@ describe("Offers", () => {
         const {data} = await socket.send("COMMON:offer-1", {});
 
         //console.log(data);
-        expect(data).equal({});
+        expect(data).to.be.an('object')
     });
 
     it("C21141 - (+) valid request offer 2", async () => {
         const {data} = await socket.send("COMMON:offer-2", {});
 
         //console.log(data);
-        expect(data).equal({});
+        expect(data).to.be.an('object')
     });
 
     it("C21142 - (+) valid request offer 3", async () => {
         const {data} = await socket.send("COMMON:offer-3", {});
 
         //console.log(data);
-        expect(data).equal({});
+        expect(data).to.be.an('object')
     });
 
 });
