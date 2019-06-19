fd420b2859d0449d3d513e7e46d5fecb1dab5139
Lina <lina.qa88@gmail.com>
2019-06-19 17:07:05 +0300

    [~] - little fix

--- test/test_match_storage2/sport_categories.js
+++ test/test_match_storage2/sport_categories.js
@@ -14,7 +14,7 @@ describe("Sport categories", () => {
         });
         //console.log(data);
         expect(data.service).equal('prematch');
-        expect(data.sportCategoriesMap["0"].categoryIconFileId).equal(null);
+        expect(data.sportCategoriesMap["0"].categoryIconFileId).not.equal(null);
         expect(data.sportCategoriesMap["0"].categoryId).not.equal(null);
         expect(data.sportCategoriesMap["0"].categoryName.en).not.equal(null);
         expect(data.sportCategoriesMap["0"].categoryName.ru).not.equal(null);
