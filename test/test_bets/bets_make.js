705a47e92cbf3fa9163dab0791f26c2151410c4d
pavgurin <kalliak1993@yandex.ru>
2019-06-21 15:17:22 +0300

    [~] merge with master

--- test/test_bets/bets_make.js
+++ test/test_bets/bets_make.js
@@ -2,28 +2,63 @@ import {expect} from 'chai';
 import {userList} from '../../src/methods/userList';
 import {register} from '../../src/methods/register';
 
-describe("Bets check", () => {
+describe('Bets check', () => {
 
     //TODO вынести методы в глобальные переменные по созданию ставки
     //TODO после обновить старые тесты с маркером dev
-    it("Bets make ordinar without money", async () => {
+
+    it('Bets make ver1', async () => {
+        const {data} = await socket.send('MATCH-STORAGE-2:sport-categories', {
+            service: 'prematch',
+            sportId: '1',
+            timeFilter: {
+                date: false,
+                hour: false
+            }
+        });
+        console.log(data);
+        const {betData} = await socket.send('BETS:bets-make',
+            {
+                'currency': 'RUB',
+                'betsMap': {
+                    '': {
+                        'amount': 13,
+                        'couponList': [
+                            {
+                                'service': 'prematch',
+                                'matchId': 16493397,
+                                'typeId': 386,
+                                'subTypeId': 0,
+                                'outCome': 'Draw / No',
+                                'specialValue': '*',
+                                'coefficient': '7.98'
+                            }
+                        ]
+                    }
+                }
+            }
+        );
+        console.log(betData);
+    });
+
+    it('Bets make ordinar without money', async () => {
         const {data: regReq} = await register.one_click_reg();
         await userList.login_with_params(regReq.email, regReq.password);
-        const {betData} = await socket.send("BETS:bets-make",
+        const {betData} = await socket.send('BETS:bets-make',
             {
-                "currency": "RUB",
-                "betsMap": {
-                    "prematch_16493397_386_0_Draw / No_*": {
-                        "amount": 13,
-                        "couponList": [
+                'currency': 'RUB',
+                'betsMap': {
+                    'prematch_16493397_386_0_Draw / No_*': {
+                        'amount': 1,
+                        'couponList': [
                             {
-                                "service": "prematch",
-                                "matchId": 16493397,
-                                "typeId": 386,
-                                "subTypeId": 0,
-                                "outCome": "Draw / No",
-                                "specialValue": "*",
-                                "coefficient": "7.98"
+                                'service': 'prematch',
+                                'matchId': 16493397,
+                                'typeId': 386,
+                                'subTypeId': 0,
+                                'outCome': 'Draw / No',
+                                'specialValue': '*',
+                                'coefficient': '7.98'
                             }
                         ]
                     }
@@ -36,23 +71,23 @@ describe("Bets check", () => {
         expect(betData).to.deep.include({message: 'Недостаточно средств'});
     });
 
-    it("Bets make ordinar with money", async () => {
+    it('Bets make ordinar with money', async () => {
         await userList.login_with_RUB();
-        const {betData} = await socket.send("BETS:bets-make",
+        const {betData} = await socket.send('BETS:bets-make',
             {
-                "currency": "RUB",
-                "betsMap": {
-                    "prematch_16493397_386_0_Draw / No_*": {
-                        "amount": 13,
-                        "couponList": [
+                'currency': 'RUB',
+                'betsMap': {
+                    'prematch_16493397_386_0_Draw / No_*': {
+                        'amount': 13,
+                        'couponList': [
                             {
-                                "service": "prematch",
-                                "matchId": 16493397,
-                                "typeId": 386,
-                                "subTypeId": 0,
-                                "outCome": "Draw / No",
-                                "specialValue": "*",
-                                "coefficient": "7.98"
+                                'service': 'prematch',
+                                'matchId': 16493397,
+                                'typeId': 386,
+                                'subTypeId': 0,
+                                'outCome': 'Draw / No',
+                                'specialValue': '*',
+                                'coefficient': '7.98'
                             }
                         ]
                     }
@@ -63,5 +98,5 @@ describe("Bets check", () => {
 
         expect(betData).to.deep.include({status: 403});
         expect(betData).to.deep.include({message: 'Недостаточно средств'});
-    })
+    });
 });
