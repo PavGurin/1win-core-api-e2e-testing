bc5020f722f697b31d7040000b5a19406d2266d2
Lina <lina.qa88@gmail.com>
2019-06-22 08:24:35 +0300

    [~] - add new tests on piastrix_rub

---
+++ test/test_banking/deposit_create/rub_valid/paymentType_piastrix_rub.js
@@ -0,0 +1,163 @@
+import {register} from '../../src/methods/register';
+import {banking} from "../../../../src/methods/banking";
+import {succses_deposit_create} from "../../../../src/expects/expect_banking";
+import {checkErrMsg} from "../../../../src/responseChecker";
+
+// beforeEach('Регистрация нового пользователя перед началом каждого теста', async () => {
+//     const {user} = await register.one_click_reg();
+//     }
+// );
+
+describe('Create deposite for piastrix_rub - RUB @master', () => {
+
+    it(' (+) amount = 100 & wallet = empty', async () => {
+        const {user} = await register.one_click_reg();
+        const {data} = await banking.deposite_create_rub(
+            100, '', 'piastrix_rub', 'RUB');
+        //console.log(data);
+        succses_deposit_create(data, 'RUB', user.id,
+            'piastrix_rub', 100)
+    });
+
+    it(' (+) amount = 100.01 & wallet = symbols', async () => {
+        const {user} = await register.one_click_reg();
+        const {data} = await banking.deposite_create_rub(
+            100.01, '123 autotests', 'piastrix_rub', 'RUB');
+        //console.log(data);
+        succses_deposit_create(data, 'RUB', user.id,
+            'piastrix_rub', 100.01)
+    });
+
+    it(' amount = 2000 & wallet = symbols', async () => {
+        const {user} = await register.one_click_reg();
+        const {data} = await banking.deposite_create_rub(
+            2000, 'порпорпорпэ', 'piastrix_rub', 'RUB');
+        //console.log(data);
+        succses_deposit_create(data, 'RUB', user.id,
+            'piastrix_rub', 2000)
+    });
+
+    it(' min amount & wallet = symbols', async () => {
+        const {user} = await register.one_click_reg();
+        const {data} = await banking.deposite_create_rub(1,
+            '123234345456 etryrt', 'piastrix_rub', 'RUB')
+
+        //console.log(data);
+        succses_deposit_create(data, 'RUB', user.id,
+            'piastrix_rub', 1)
+    });
+
+    it('> min amount & wallet = symbols', async () => {
+        const {user} = await register.one_click_reg();
+        const {data} = await banking.deposite_create_rub(2,
+            '12№%:№%:45456etryrt', 'piastrix_rub', 'RUB')
+
+        //console.log(data);
+        succses_deposit_create(data, 'RUB', user.id,
+            'piastrix_rub', 2)
+    });
+
+    it(' max amount & wallet = numbers', async () => {
+        const {user} = await register.one_click_reg();
+        const {data} = await banking.deposite_create_rub(100000, '09090909999',
+            'piastrix_rub', 'RUB')
+        //console.log(data);
+        succses_deposit_create(data, 'RUB', user.id,
+            'piastrix_rub', 100000)
+    });
+
+    it('< max amount & wallet = numbers', async () => {
+        const {user} = await register.one_click_reg();
+        const {data} = await banking.deposite_create_rub(99999, '0[[[?<><?999',
+            'piastrix_rub', 'RUB')
+        //console.log(data);
+        succses_deposit_create(data, 'RUB', user.id,
+            'piastrix_rub', 99999)
+    });
+
+    //Не знаю, какой должен быть результат
+    it(' without currency', async () => {
+        const {user} = await register.one_click_reg();
+        const {data1} = await banking.deposite_create_rub(100, undefined,
+            'piastrix_rub', 'RUB')
+        //console.log(data);
+        succses_deposit_create(data, 'RUB', user.id,
+            'piastrix_rub', 100)
+    });
+
+    it(' without currency', async () => {
+        const {user} = await register.one_click_reg();
+        const {data} = await socket.send('BANKING:deposit-create', {
+            amount: '100',
+            wallet: '00001111222223333',
+            paymentType: 'piastrix_rub',
+        });
+        //console.log(data);
+        succses_deposit_create(data, 'RUB', user.id,
+            'piastrix_rub', 100)
+    });
+});
+
+describe('Create deposite for piastrix_rub invalid - RUB', () => {
+
+    it(' amount = 0', async () => {
+        const {user} = await register.one_click_reg();
+        const {data1} = await banking.deposite_create_rub(0, '',
+            'piastrix_rub', 'RUB')
+        //console.log(data);
+        checkErrMsg(400, 'Неверная сумма')
+    });
+
+    it(' amount < min amount', async () => {
+        const {user} = await register.one_click_reg();
+        const {data1} = await banking.deposite_create_rub(0.6, '',
+            'piastrix_rub', 'RUB')
+        //console.log(data);
+        checkErrMsg(400, 'Неверная сумма')
+    });
+
+    it(' amount > max amount', async () => {
+        const {user} = await register.one_click_reg();
+        const {data1} = await banking.deposite_create_rub(100001, '',
+            'piastrix_rub', 'RUB')
+        //console.log(data);
+        checkErrMsg(400, 'Неверная сумма')
+    });
+
+    it(' amount > max amount', async () => {
+        const {user} = await register.one_click_reg();
+        const {data1} = await banking.deposite_create_rub(100000.56, '',
+            'piastrix_rub', 'RUB')
+        //console.log(data);
+        checkErrMsg(400, 'Неверная сумма')
+    });
+
+    it(' wallet = null', async () => {
+        const {user} = await register.one_click_reg();
+        const {data1} = await banking.deposite_create_rub(1, null,
+            'piastrix_rub', 'RUB')
+        //console.log(data);
+        checkErrMsg(400, 'Неверный формат кошелька')
+    });
+
+    it(' wallet = long string', async () => {
+        const {user} = await register.one_click_reg();
+        const {data1} = await banking.deposite_create_rub(1,
+            //TODO посмотреть количество символов доступных в кошельке
+            '1231231231231231453453345345342312312312312123123123123',
+            'piastrix_rub', 'RUB')
+        //console.log(data);
+        checkErrMsg(400, 'Неверный формат кошелька')
+    });
+
+    //Не знаю что тут должно быть
+    it(' incorrect paymentType = piastrix_rub_test', async () => {
+        const {user} = await register.one_click_reg();
+        const {data1} = await banking.deposite_create_rub(1,
+            //TODO посмотреть количество символов доступных в кошельке
+            '1231231231231231453453345345342312312312312123123123123',
+            'piastrix_rub_test', 'RUB')
+        //console.log(data);
+        checkErrMsg(400, '?????')
+    });
+});
