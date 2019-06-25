2e7ddf3032712e7278087fe0c83e689d9b253700
Yulia Gazizova <gazizovayu94@gmail.com>
2019-06-25 16:40:54 +0300

    [+] added test on results date filter

---
+++ test/test_results/results.js
@@ -0,0 +1,32 @@
+import {expect} from "chai"
+
+describe("Results", () => {
+
+    //форматирование даты для отправки в виде дд/мм
+    //daysAgo - дата за сколько дней назад от текущего будет получена
+    //пример: getFormattedDate(0) - сегодняшняя дата, getFormattedDate(1) - вчерашняя
+    //TODO вынести из теста
+    function getFormattedDate(daysAgo) {
+        var date = new Date();
+        date.setDate(date.getDate() - daysAgo);
+        return date.getDate() + "/" + (date.getMonth() + 1);
+
+    }
+
+    for(var i = 0; i <=6; i++ ) {
+        var date = getFormattedDate(i);
+        it("C22707 - (+) results with date filtration, date: " + date, async () => {
+            const {data} = await socket.send("RESULT:results-all", {
+                timeFilter: {
+                    date: date,
+                    hoursToStart: false
+                }
+            })
+            //console.log(data);
+            expect(data).to.be.an('object');
+            //TODO добавить нормальных проверок
+            //expect(data[1].tournamentMap[1].matchMap[1].dateOfMatch).to.be.above(date.getUnixTimestamp());
+            //expect(data[1].tournamentMap[1].matchMap[1].dateOfMatch).to.be.below((date.setDate(date.getDate()+1)).getUnixTimestamp());
+        })
+    }
+})
