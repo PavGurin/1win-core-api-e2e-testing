bb2fc2162f765459de77910f9c0ac788d3bf9eb2
pavgurin <kalliak1993@yandex.ru>
2019-07-08 12:37:01 +0300

    [-] middle changes (not final version

--- test/bets/betsMake.js
+++ test/bets/betsMake.js
@@ -1,27 +1,33 @@
-import {expect} from 'chai';
-import {userList} from '../../src/methods/userList';
-import {generateCoupon, getTournamentMatches, makeBet} from '../../src/methods/better';
+import { expect } from 'chai';
+import { userList } from '../../src/methods/userList';
+import {
+  generateCoupon, getSport, getTournamentMatches, makeBet,
+} from '../../src/methods/better';
 
 describe('Bets make', () => {
-
-  //TODO вынести методы в глобальные переменные по созданию ставки
-  //TODO после обновить старые тесты с маркером dev
+  // TODO вынести методы в глобальные переменные по созданию ставки
+  // TODO после обновить старые тесты с маркером dev
 
   beforeEach(async () => {
-    await userList.login_with_real_money();
+    await userList.loginWithRealMoney();
   });
 
   it('Prematch - ordinary bet', async () => {
-
     /**
-     get available matches
-     service 'live/prematch'
-     sportId => (football = 1)
-     */
+         get available matches
+         service 'live/prematch'
+         sportId => (football = 1)
 
-    const {data: matches} = await getTournamentMatches({
-      service: 'prematch'
-      // sportId: '1'
+
+         sport-all
+         sport-categories
+         sport-tournaments
+         tournament-matches
+         */
+
+    const { data: matches } = await getTournamentMatches({
+      service: 'prematch',
+      sportId: '1',
     });
 
     const coupon = await generateCoupon(matches);
@@ -31,52 +37,70 @@ describe('Bets make', () => {
     // console.log(betResponse);
   });
 
-  it('Prematch - express bet', async () => {
-
-    /**
-     get available matches
-     service 'live/prematch'
-     sportId => (football = 1)
-     */
-
-    const {data: matches} = await getTournamentMatches({
-      service: 'prematch'
-      // sportId: '1'
+  it('spor1t ', async () => {
+    const { data: { sportMap } } = await socket.send('MATCH-STORAGE-2:sport-all', {
+      service: 'prematch',
+      timeFilter: {
+        date: false,
+        hour: false,
+      },
     });
-
-    const coupon = await generateCoupon(matches);
-
-    const betResponse = await makeBet(coupon);
-
-    // console.log(betResponse);
+    console.log(sportMap);
   });
 
-  it.skip('Bets make ordinary with money', async () => {
-    await userList.login_with_RUB();
-    const {betData} = await socket.send('BETS:bets-make',
-        {
-          'currency': 'RUB',
-          'betsMap': {
-            'prematch_16493397_386_0_Draw / No_*': {
-              'amount': 13,
-              'couponList': [
-                {
-                  'service': 'prematch',
-                  'matchId': 16493397,
-                  'typeId': 386,
-                  'subTypeId': 0,
-                  'outCome': 'Draw / No',
-                  'specialValue': '*',
-                  'coefficient': '7.98'
-                }
-              ]
-            }
-          }
-        }
-    );
-    //console.log(betData);
 
-    expect(betData).to.deep.include({status: 403});
-    expect(betData).to.deep.include({message: 'Недостаточно средств'});
+  it('sport ', async () => {
+    const { data: { topTournaments } } = await getSport({
+      sportId: 1,
+    });
+    console.log(topTournaments);
   });
+
+
+  // it('Prematch - express bet', async () => {
+  //   /**
+  //        get available matches
+  //        service 'live/prematch'
+  //        sportId => (football = 1)
+  //        */
+  //
+  //   const { data: matches } = await getTournamentMatches({
+  //     service: 'prematch',
+  //     // sportId: '1'
+  //   });
+  //
+  //   const coupon = await generateCoupon(matches);
+  //
+  //   const betResponse = await makeBet(coupon);
+  //
+  //   // console.log(betResponse);
+  // });
+  //
+  // it.skip('Bets make ordinary with money', async () => {
+  //   await userList.login_with_RUB();
+  //   const { betData } = await socket.send('BETS:bets-make',
+  //     {
+  //       currency: 'RUB',
+  //       betsMap: {
+  //         'prematch_16493397_386_0_Draw / No_*': {
+  //           amount: 13,
+  //           couponList: [
+  //             {
+  //               service: 'prematch',
+  //               matchId: 16493397,
+  //               typeId: 386,
+  //               subTypeId: 0,
+  //               outCome: 'Draw / No',
+  //               specialValue: '*',
+  //               coefficient: '7.98',
+  //             },
+  //           ],
+  //         },
+  //       },
+  //     });
+  //   // console.log(betData);
+  //
+  //   expect(betData).to.deep.include({ status: 403 });
+  //   expect(betData).to.deep.include({ message: 'Недостаточно средств' });
+  // });
 });
