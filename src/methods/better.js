f0713fef42064298a95e8ffad2e4e8781f47b16e
pavgurin <kalliak1993@yandex.ru>
2019-07-08 14:10:55 +0300

    [~] bets

--- src/methods/better.js
+++ src/methods/better.js
@@ -7,44 +7,54 @@ export async function getTournamentMatches(filters) {
     timeFilter: {
       date: false,
       hour: false,
-      ...filters
-    }
+        ...filters
+    },
   });
 }
 
-export async function getSport(filters) {
-  return await socket.send('MATCH-STORAGE-2:sport-top-tournaments', {
-    service: 'prematch',
-    ...filters
+export async function sportTournaments(serviceType, sport_Id) {
+    return await socket.send('MATCH-STORAGE-2:sport-tournaments', {
+        service: serviceType,
+        sportId: sport_Id
+    });
+}
 
-  });
+// USELESS for bets
+export async function sportAll(serviceType) {
+    return await socket.send('MATCH-STORAGE-2:sport-all', {
+        service: serviceType,
+        timeFilter: {
+            date: false,
+            hour: false
+        }
+    });
 }
 
 export function generateCoupon(matches) {
   // const singleMatch = Object.values(matches.matchMap)[0];
   // const odd = singleMatch.baseOddsConfig[0].cellList[0].odd;
-  return new Coupon({...Object.values(matches.matchMap)[0].baseOddsConfig[0].cellList[0].odd});
+    return new Coupon({...Object.values(matches.matchMap)[0].baseOddsConfig[0].cellList[0].odd});
 }
 
 export async function makeBet(coupon) {
   return await socket.send('BETS:bets-make',
       {
-        currency: 'RUB',
-        betsMap: {
-          [coupon.couponId]: {
-            amount: 1,
-            couponList: [
-              {
-                service: coupon.service,
-                matchId: coupon.matchId,
-                typeId: coupon.typeId,
-                subTypeId: coupon.subTypeId,
-                outCome: coupon.outCome,
-                specialValue: coupon.specialValue,
-                coefficient: coupon.saveCoefficient
+          currency: 'RUB',
+          betsMap: {
+              [coupon.couponId]: {
+                  amount: 1,
+                  couponList: [
+                      {
+                          service: coupon.service,
+                          matchId: coupon.matchId,
+                          typeId: coupon.typeId,
+                          subTypeId: coupon.subTypeId,
+                          outCome: coupon.outCome,
+                          specialValue: coupon.specialValue,
+                          coefficient: coupon.saveCoefficient
+                      }
+                  ]
               }
-            ]
           }
-        }
       });
 }
--- test/bets/betsMake.js
+++ test/bets/betsMake.js
@@ -1,8 +1,5 @@
-import { expect } from 'chai';
-import { userList } from '../../src/methods/userList';
-import {
-  generateCoupon, getSport, getTournamentMatches, makeBet,
-} from '../../src/methods/better';
+import {userList} from '../../src/methods/userList';
+import {generateCoupon, getTournamentMatches, makeBet, sportAll, sportTournaments} from '../../src/methods/better';
 
 describe('Bets make', () => {
   // TODO вынести методы в глобальные переменные по созданию ставки
@@ -12,29 +9,46 @@ describe('Bets make', () => {
     await userList.loginWithRealMoney();
   });
 
+  /**
+   get available matches
+   service 'live/prematch'
+   sportId => (football = 1)
+
+
+   sport-all
+   sport-categories
+   sport-tournaments
+   tournament-matches
+   */
+
+  it('re', async () => {
+    const {data: {sportMap}} = await sportAll('prematch');
+    const coupon = await generateCoupon(sportMap);
+    const betReponse = await makeBet(coupon);
+    console.log(betReponse);
+  });
+
+  it('re1', async () => {
+    const {data: {sportTournamentMap}} = await sportTournaments('prematch', 1);
+    console.log(sportTournamentMap);
+    const coupon = await generateCoupon(sportTournamentMap);
+    const betResponse = await makeBet(coupon);
+    console.log(betResponse);
+  });
+
   it('Prematch - ordinary bet', async () => {
-    /**
-         get available matches
-         service 'live/prematch'
-         sportId => (football = 1)
-
-
-         sport-all
-         sport-categories
-         sport-tournaments
-         tournament-matches
-         */
-
     const { data: matches } = await getTournamentMatches({
       service: 'prematch',
-      sportId: '1',
+      tournamentId: 12
     });
 
     const coupon = await generateCoupon(matches);
 
+    console.log(coupon);
+
     const betResponse = await makeBet(coupon);
 
-    // console.log(betResponse);
+    console.log(betResponse);
   });
 
   it('spor1t ', async () => {
@@ -50,7 +64,7 @@ describe('Bets make', () => {
 
 
   it('sport ', async () => {
-    const { data: { topTournaments } } = await getSport({
+    const {data: {topTournaments}} = await sportAll({
       sportId: 1,
     });
     console.log(topTournaments);
