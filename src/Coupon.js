d62d7125a258e587bcc99e31d4961b6020821d02
PavelGurin <kalliak1993@yandex.ru>
2019-07-15 15:46:28 +0300

    запросы ставок и запросы списка матчей раскиданы по отдельным файлам
    добавлены тесты одиночной ставки, экспресс, максимальнойСуммыСтавки
    добавлены тесты старых запросов для сравнения с нововведенным микросервисом

--- src/Coupon.js
+++ src/Coupon.js
@@ -1,11 +1,3 @@
-// eslint-disable-next-line no-unused-vars
-const [NOT_STATUS, TYPE_SUCCESS, TYPE_ERROR, TYPE_ERROR_INFO] = [
-  'not',
-  'success',
-  'error',
-  'error-info',
-];
-
 export default class Coupon {
   constructor(data = {}) {
     const {
@@ -15,9 +7,6 @@ export default class Coupon {
       subTypeId = '',
       outCome = '',
       specialValue = '',
-      oddsType = null,
-      odd = null,
-      match = null,
       coefficient = null,
     } = data;
 
@@ -36,18 +25,7 @@ export default class Coupon {
     this.subTypeId = subTypeId;
     this.outCome = outCome;
     this.specialValue = specialValue;
-    this.oddsType = oddsType;
-    this.odd = odd;
-    this.match = match;
     this.saveCoefficient = +coefficient;
-
-    // old
-    this.maxBetAmount = 0;
-    this.sum = 100;
-    this.marketName = '';
-    this.status = NOT_STATUS;
-    this.dataResponse = {};
-    this.maxBetAmountLoading = true;
   }
 
   static genId({
--- test/bets/betsMake.js
+++ test/bets/betsMake.js
@@ -1,73 +1,352 @@
 import { expect } from 'chai';
+
 import { userList } from '../../src/methods/userList';
-import { generateCoupon, getTournamentMatches, makeBet } from '../../src/methods/better';
+import {
+  generateExpressCoupon,
+  generateOrdinaryCoupon,
+  getMaxBetAmount,
+  getMaxBetAmountOld,
+  makeExpressBet,
+  makeOrdinaryBet,
+  makeOrdinaryBetOld,
+} from '../../src/methods/better';
 
-describe('Bets make', () => {
-  // TODO вынести методы в глобальные переменные по созданию ставки
-  // TODO после обновить старые тесты с маркером dev
+import { getSingleMatch, sportTournaments, tournamentMatches } from '../../src/methods/matchStorage';
 
-  beforeEach(async () => {
-    await userList.loginWithRealMoney();
+const currency = 'RUB';
+const PREMATCH = 'prematch';
+const LIVE = 'live';
+
+beforeEach(async () => {
+  await userList.loginWithRealMoney();
+});
+
+describe('Ordinary bets prematch', () => {
+  it('C27551 (+) default bet', async () => {
+    const [singleMatch] = await getSingleMatch(PREMATCH);
+    console.log(singleMatch);
+
+    const coupon = generateOrdinaryCoupon(singleMatch, currency, 1);
+    // console.log(coupon);
+
+    const betResponse = await makeOrdinaryBet(coupon, currency, 1);
+    console.log(betResponse);
+
+    expect(betResponse.data[coupon.couponId].error).equal(false);
+    expect(betResponse.status).equal(200);
   });
 
-  it('Prematch - ordinary bet', async () => {
-    /**
-     get available matches
-     service 'live/prematch'
-     sportId => (football = 1)
-     */
+  it('C27552 (-) changed matchId', async () => {
+    const [singleMatch] = await getSingleMatch(PREMATCH);
+    // console.log(singleMatch);
 
-    const { data: matches } = await getTournamentMatches({
-      service: 'prematch',
-      // sportId: '1'
-    });
-    const coupon = await generateCoupon(matches);
-    const betResponse = await makeBet(coupon);
-
+    const coupon = generateOrdinaryCoupon(singleMatch);
+    // console.log(coupon);
+    coupon.matchId += 1;
+    const betResponse = await makeOrdinaryBet(coupon, currency, 1);
     // console.log(betResponse);
+
+    expect(betResponse.data[coupon.couponId].status).equal(400);
+    expect(betResponse.data[coupon.couponId].error.errorMessage)
+      .equal(`Match ${coupon.matchId} not found at service prematch`);
+    expect(betResponse.status).equal(200);
   });
 
-  it('Prematch - express bet', async () => {
-    /**
-     get available matches
-     service 'live/prematch'
-     sportId => (football = 1)
-     */
+  it('C27553 (-) changed service', async () => {
+    const [singleMatch] = await getSingleMatch(PREMATCH);
+    // console.log(singleMatch);
 
-    const { data: matches } = await getTournamentMatches({
-      service: 'prematch',
-      // sportId: '1'
-    });
-    const coupon = await generateCoupon(matches);
-    const betResponse = await makeBet(coupon);
-
+    const coupon = generateOrdinaryCoupon(singleMatch);
+    // console.log(coupon);
+    coupon.service = LIVE;
+    const betResponse = await makeOrdinaryBet(coupon, currency, 3);
     // console.log(betResponse);
+
+    expect(betResponse.data[coupon.couponId].status).equal(400);
+    expect(betResponse.data[coupon.couponId].error.errorMessage)
+      .equal(`Match ${coupon.matchId} not found at service ${LIVE}`);
+    expect(betResponse.status).equal(200);
   });
 
-  it.skip('Bets make ordinary with money', async () => {
-    await userList.loginWithRub();
-    const { betData } = await socket.send('BETS:bets-make',
-      {
-        currency: 'RUB',
-        betsMap: {
-          'prematch_16493397_386_0_Draw / No_*': {
-            amount: 13,
-            couponList: [
-              {
-                service: 'prematch',
-                matchId: 16493397,
-                typeId: 386,
-                subTypeId: 0,
-                outCome: 'Draw / No',
-                specialValue: '*',
-                coefficient: '7.98',
-              },
-            ],
-          },
-        },
-      });
-    // console.log(betData);
-    expect(betData).to.deep.include({ status: 403 });
-    expect(betData).to.deep.include({ message: 'Недостаточно средств' });
+  it('C27554 (bug) changed coefficient', async () => {
+    const [singleMatch] = await getSingleMatch(PREMATCH);
+    // console.log(singleMatch);
+
+    const coupon = generateOrdinaryCoupon(singleMatch);
+    // console.log(coupon);
+    coupon.saveCoefficient = 2;
+    const betResponse = await makeOrdinaryBet(coupon, currency, 1);
+    // console.log(betResponse);
+
+    expect(betResponse.data[coupon.couponId].status).equal(400);
+    expect(betResponse.data[coupon.couponId].error.errorMessage).equal('Odds higher than market in Line Service');
+    expect(betResponse.status).equal(200);
+  });
+
+  it('C27555 (-) changed typeID', async () => {
+    const [singleMatch] = await getSingleMatch(PREMATCH);
+    // console.log(singleMatch);
+
+    const coupon = generateOrdinaryCoupon(singleMatch);
+    // console.log(coupon);
+    coupon.typeId += 1;
+    const betResponse = await makeOrdinaryBet(coupon, currency, 3);
+    // console.log(betResponse);
+
+    expect(betResponse.data[coupon.couponId].status).equal(400);
+    expect(betResponse.data[coupon.couponId].error.errorMessage).equal('Requested odd not found');
+    expect(betResponse.status).equal(200);
+  });
+
+  it('C27556 (-) changed outCome', async () => {
+    const [singleMatch] = await getSingleMatch(PREMATCH);
+    // console.log(singleMatch);
+
+    const coupon = generateOrdinaryCoupon(singleMatch);
+    // console.log(coupon);
+    coupon.outCome += 1;
+    const betResponse = await makeOrdinaryBet(coupon, currency, 3);
+    // console.log(betResponse);
+
+    expect(betResponse.data[coupon.couponId].status).equal(400);
+    expect(betResponse.data[coupon.couponId].error.errorMessage).equal('Requested odd not found');
+    expect(betResponse.status).equal(200);
+  });
+
+  it('C27557 (-) changed specialValue', async () => {
+    const [singleMatch] = await getSingleMatch(PREMATCH);
+    // console.log(singleMatch);
+
+    const coupon = generateOrdinaryCoupon(singleMatch);
+    // console.log(coupon);
+    coupon.specialValue += 1;
+    const betResponse = await makeOrdinaryBet(coupon, currency, 3);
+    // console.log(betResponse);
+
+    expect(betResponse.data[coupon.couponId].status).equal(400);
+    expect(betResponse.data[coupon.couponId].error.errorMessage).equal('Requested odd not found');
+    expect(betResponse.status).equal(200);
+  });
+});
+
+describe('Ordinary bets live', () => {
+  // TODO add to getSingleMatch status of returned match
+  it('C27564 (+) default bet', async () => {
+    const [singleMatch] = await getSingleMatch(LIVE);
+    // console.log(singleMatch);
+
+    const coupon = generateOrdinaryCoupon(singleMatch);
+    // console.log(coupon);
+
+    const betResponse = await makeOrdinaryBet(coupon, currency, 1);
+    // console.log(betResponse);
+
+    expect(betResponse.data[coupon.couponId].error).equal(false);
+    expect(betResponse.status).equal(200);
+  });
+
+  it('C27565 (-) changed matchId', async () => {
+    const [singleMatch] = await getSingleMatch(LIVE);
+    // console.log(singleMatch);
+
+    const coupon = generateOrdinaryCoupon(singleMatch);
+    // console.log(coupon);
+    coupon.matchId += 1;
+    const betResponse = await makeOrdinaryBet(coupon, currency, 1);
+    // console.log(betResponse);
+
+    expect(betResponse.data[coupon.couponId].status).equal(400);
+    expect(betResponse.data[coupon.couponId].error.errorMessage)
+      .equal(`Match ${coupon.matchId} not found at service live`);
+    expect(betResponse.status).equal(200);
+  });
+
+  it('C27566 (-) changed service', async () => {
+    const [singleMatch] = await getSingleMatch(LIVE);
+    // console.log(singleMatch);
+
+    const coupon = generateOrdinaryCoupon(singleMatch);
+    // console.log(coupon);
+    coupon.service = PREMATCH;
+    const betResponse = await makeOrdinaryBet(coupon, currency, 3);
+    // console.log(betResponse);
+
+    expect(betResponse.data[coupon.couponId].status).equal(400);
+    expect(betResponse.data[coupon.couponId].error.errorMessage)
+      .equal(`Match ${coupon.matchId} not found at service ${PREMATCH}`);
+    expect(betResponse.status).equal(200);
+  });
+
+  it('C27567 (bug) changed coefficient', async () => {
+    const [singleMatch] = await getSingleMatch(LIVE);
+    // console.log(singleMatch);
+
+    const coupon = generateOrdinaryCoupon(singleMatch);
+    // console.log(coupon);
+    coupon.saveCoefficient = 55;
+    const betResponse = await makeOrdinaryBet(coupon, currency, 1);
+    console.log(betResponse);
+
+    expect(betResponse.status).equal(200);
+    // expect(betResponse.data[coupon.couponId].error).to.not.equal(false);
+    expect(betResponse.data[coupon.couponId].error.errorMessage).equal('Odds higher than market in Line Service');
+  });
+
+  it('C27568 (-) changed typeID', async () => {
+    const [singleMatch] = await getSingleMatch(LIVE);
+    // console.log(singleMatch);
+
+    const coupon = generateOrdinaryCoupon(singleMatch);
+    // console.log(coupon);
+    coupon.typeId += 1;
+    const betResponse = await makeOrdinaryBet(coupon, currency, 3);
+    console.log(betResponse);
+
+    expect(betResponse.data[coupon.couponId].status).equal(400);
+    expect(betResponse.data[coupon.couponId].error.errorMessage).equal('Requested odd not found');
+    expect(betResponse.status).equal(200);
+  });
+
+  it('C27569 (-) changed outCome', async () => {
+    const [singleMatch] = await getSingleMatch(LIVE);
+    // console.log(singleMatch);
+
+    const coupon = generateOrdinaryCoupon(singleMatch);
+    // console.log(coupon);
+    coupon.outCome += 1;
+    const betResponse = await makeOrdinaryBet(coupon, currency, 3);
+    console.log(betResponse);
+
+    expect(betResponse.data[coupon.couponId].status).equal(400);
+    expect(betResponse.data[coupon.couponId].error.errorMessage).equal('Requested odd not found');
+    expect(betResponse.status).equal(200);
+  });
+
+  it('C27570 (-) changed specialValue', async () => {
+    const [singleMatch] = await getSingleMatch(LIVE);
+    // console.log(singleMatch);
+
+    const coupon = generateOrdinaryCoupon(singleMatch);
+    // console.log(coupon);
+    coupon.specialValue += 1;
+    const betResponse = await makeOrdinaryBet(coupon, currency, 3);
+    console.log(betResponse);
+
+    expect(betResponse.data[coupon.couponId].status).equal(400);
+    expect(betResponse.data[coupon.couponId].error.errorMessage).equal('Requested odd not found');
+    expect(betResponse.status).equal(200);
+  });
+});
+
+describe('Old bets requests', () => {
+  it('C27558 (+) liveOld', async () => {
+    const [singleMatch] = await getSingleMatch(LIVE);
+    // console.log(singleMatch);
+
+    const coupon = generateOrdinaryCoupon(singleMatch);
+    // console.log(coupon);
+
+    const betResponse = await makeOrdinaryBet(coupon, currency, 1);
+    // console.log(betResponse);
+
+    expect(betResponse.data[coupon.couponId].error).equal(false);
+    expect(betResponse.status).equal(200);
+  });
+
+  it('C27559 (-) Ordinary bet OLD changed coefficient', async () => {
+    const [singleMatch] = await getSingleMatch(PREMATCH);
+    // console.log(singleMatch);
+
+    const coupon = generateOrdinaryCoupon(singleMatch);
+    // console.log(coupon);
+    coupon.saveCoefficient = 10;
+    const betResponse = await makeOrdinaryBetOld(coupon, currency, 1);
+    // console.log(betResponse);
+
+    expect(betResponse.status).equal(200);
+    expect(betResponse[coupon.couponId].error.errorMessage).equal('Odds higher than market in Line Service');
+    expect(betResponse[coupon.couponId].status).equal(400);
+  });
+
+  it('C27560 (+) MaxBetAmount OLD', async () => {
+    const betAmount = 1;
+
+    const [singleMatch] = await getSingleMatch(PREMATCH);
+    // console.log(singleMatch);
+
+    const coupon = generateOrdinaryCoupon(singleMatch);
+    // console.log(coupon);
+
+    const data = await getMaxBetAmountOld(coupon, singleMatch);
+    // console.log(data.maxBetAmount);
+
+    const bet = await makeOrdinaryBetOld(coupon, currency, betAmount);
+    // console.log(bet);
+
+    const data2 = await getMaxBetAmountOld(coupon, singleMatch);
+    // console.log(data2.maxBetAmount);
+
+    expect(data2.maxBetAmount).equal(data.maxBetAmount - betAmount);
+  });
+});
+
+
+describe('maxBetAmount', () => {
+  it('C27561 (+) MaxBetAmount', async () => {
+    const betAmount = 1;
+
+    const [singleMatch] = await getSingleMatch(PREMATCH);
+    // console.log(singleMatch);
+
+    const coupon = generateOrdinaryCoupon(singleMatch);
+    // console.log(coupon);
+
+    const data = await getMaxBetAmount(coupon, singleMatch);
+    console.log(data);
+
+    const bet = await makeOrdinaryBet(coupon, currency, betAmount);
+    // console.log(bet);
+    // coupon = generateOrdinaryCoupon(singleMatch);
+
+    const data2 = await getMaxBetAmount(coupon, singleMatch);
+    console.log(data2);
+
+    expect(data2.maxBetAmount).equal(data.maxBetAmount - betAmount);
+  });
+});
+
+describe('Express', () => {
+  it('C27562 Prematch - express bet', async () => {
+    const { data: { sportTournamentMap } } = await sportTournaments(PREMATCH, 'all');
+    // console.log(sportTournamentMap);
+
+    const { data: { matchMap } } = await tournamentMatches(
+      PREMATCH,
+      Object.values(Object.values(sportTournamentMap)[0])[0].tournamentId,
+    );
+
+    const coupon = generateExpressCoupon(matchMap, 2, 1);
+
+    const betResponse = await makeExpressBet(Object.values(coupon));
+    // console.log(betResponse);
+    expect(betResponse.data[Object.keys(coupon.betsMap)[0]].error).equal(false);
+    expect(betResponse.status).equal(200);
+  });
+
+  it('C27563 Prematch - express bet 100', async () => {
+    const { data: { sportTournamentMap } } = await sportTournaments(PREMATCH, 'all');
+    // console.log(sportTournamentMap);
+
+    const { data: { matchMap } } = await tournamentMatches(
+      PREMATCH,
+      Object.values(Object.values(sportTournamentMap)[0])[0].tournamentId,
+    );
+
+    const coupon = generateExpressCoupon(matchMap, 100, 1);
+
+    const betResponse = await makeExpressBet(Object.values(coupon));
+    // console.log(betResponse);
+    expect(betResponse.data[Object.keys(coupon.betsMap)[0]].error).equal(false);
+    expect(betResponse.status).equal(200);
   });
 });
