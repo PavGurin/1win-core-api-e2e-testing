db52d2cf09e25489e8878df56b79928ba149d47a
Angelina <lina.qa88@gmail.com>
2019-07-25 14:09:02 +0300

    Resolve "Health checker I"

---
+++ test/health/checkResult.js
@@ -0,0 +1,18 @@
+import { expect } from 'chai';
+import { getFormattedDate } from '../../src/methods/utils';
+
+describe('Results', () => {
+  it('C22707 - (+) results with date filtration', async () => {
+    // for (let i = 0; i <= 6; i++) {
+    const date = getFormattedDate();
+    // eslint-disable-next-line no-await-in-loop
+    const { data } = await socket.send('RESULT:results-all', {
+      timeFilter: {
+        date,
+        hoursToStart: false,
+      },
+    });
+    // console.log(data);
+    expect(data).to.be.an('object');
+  });
+});
