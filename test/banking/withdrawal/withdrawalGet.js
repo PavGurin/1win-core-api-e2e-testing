54befbf16dd161c48abdfd8309f6ebf4511334a8
Angelina <lina.qa88@gmail.com>
2019-08-19 15:10:36 +0300

    Resolve "Banking: delete hardcore in withdrawal get"

--- test/banking/withdrawal/withdrawalGet.js
+++ test/banking/withdrawal/withdrawalGet.js
@@ -1,45 +1,82 @@
 import { expect } from 'chai';
 import { userList } from '../../../src/methods/userList';
 import { checkErrMsg } from '../../../src/responseChecker';
-import { userForAutoConfirm } from '../../../src/methods/userForAutoConfirm';
+import { userPool } from '../../../src/methods/userPool';
+import { logOut } from '../../../src/methods/user';
+import { banking } from '../../../src/methods/banking';
+import { sleep } from '../../../src/methods/utils';
+import { mail } from '../../../src/methods/mail';
 
 describe('Withdrawal get', () => {
-  // TODO изменить тесты - сделать вывод и получить этот id
-  // TODO тесты зависимы от окружения
-  it('C19361 (-) Get - withdrawal not found - auth', async () => {
-    await userList.loginWithRub();
-    const { data } = await socket.send('BANKING:withdrawal-get', { id: 205 });
-    // console.log(data);
-    checkErrMsg(data, 404, 'Выплата не найдена');
+  const USERS_NUMBER = 2;
+  const BALANCE = 120;
+  let receivedMail = {};
+  let currentUser = {};
+  let users = [];
+
+  beforeAll(async () => {
+    // формируем пул юзеров
+    users = await userPool.usersWithBalanceRubAndConfirmCodes(USERS_NUMBER, BALANCE);
   });
 
-  it('C19362 (+) Get - 100 RUB money-transfer - @dev ', async () => {
-    await userForAutoConfirm.EmailBk();
-    const { data } = await socket.send('BANKING:withdrawal-get', { id: 1553 });
-    // console.log(data);
-    expect(data.id).equal(1553);
-    expect(data.time).equal(1563460751000);
-    expect(data.payment_system).equal('money-transfer');
-    expect(data.amount).equal(34);
-    expect(data.status).equal(1);
-    expect(data.wallet).equal('188');
+  describe('Invalid Id withdrawal', () => {
+    it('C19364 (-) Get - Bad request, id is required ', async () => {
+      const { data } = await socket.send('BANKING:withdrawal-get', { id: null });
+      // console.log(data);
+      checkErrMsg(data, 400, 'Bad request, id is required');
+    });
+
+    it('C19361 (-) Get - withdrawal not found - auth', async () => {
+      const { data } = await socket.send('BANKING:withdrawal-get', { id: 0 });
+      // console.log(data);
+      checkErrMsg(data, 404, 'Выплата не найдена');
+    });
   });
 
-  it('C19363 (+) Get - 100 RUB card_rub @dev', async () => {
-    await userForAutoConfirm.EmailBk();
-    const { data } = await socket.send('BANKING:withdrawal-get', { id: 1560 });
-    // console.log(data);
-    expect(data.id).equal(1560);
-    expect(data.time).equal(1563460814000);
-    expect(data.payment_system).equal('card_rub');
-    expect(data.amount).equal(100);
-    expect(data.status).equal(0);
-    expect(data.wallet).equal('0000111122223333');
-  });
+  describe('Withdrawal get card_rub', () => {
+    beforeEach(async () => {
+      await logOut();
+      currentUser = users.pop();
+      await userList.loginWithParams(currentUser.email, currentUser.password);
+    });
 
-  it('C19364 (-) Get - Bad request, id is required ', async () => {
-    const { data } = await socket.send('BANKING:withdrawal-get', { id: null });
-    // console.log(data);
-    checkErrMsg(data, 400, 'Bad request, id is required');
+    it('C19363 (+) Get - 100 RUB card_rub ', async () => {
+      await banking.withdrawalCreate(100, '5469550073662048', 'card_rub', 'RUB');
+
+      // задержка для получения письма
+      await sleep(4000);
+      receivedMail = await mail.getMessage(currentUser.email);
+      const confirm = await socket.send('BANKING:withdrawal-confirm', { code: receivedMail.code });
+
+      const { data } = await socket.send('BANKING:withdrawal-get', { id: confirm.data.id });
+      // console.log(data);
+
+      expect(data.id).equal(confirm.data.id);
+      expect(data.time).to.be.below(new Date().getTime());
+      expect(data.payment_system).equal('card_rub');
+      expect(data.amount).equal(100);
+      expect(data.status).equal(0);
+      expect(data.wallet).equal('5469550073662048');
+    });
+
+
+    it('C19362 (+) Get - 100 RUB money-transfer ', async () => {
+      await banking.transferCreate(20, 'RUB');
+      // задержка, чтобы письмо успело придти на почту
+      await sleep(4000);
+      receivedMail = await mail.getMessage(currentUser.email);
+      await socket.send('BANKING:transfer-confirm', { code: receivedMail.code });
+
+      const data1 = await socket.send('BANKING:withdrawal-history');
+
+      const { data } = await socket.send('BANKING:withdrawal-get', { id: data1.data[0].id });
+
+      expect(data.id).equal(data1.data[0].id);
+      expect(data.time).to.be.below(new Date().getTime());
+      expect(data.payment_system).equal('money-transfer');
+      expect(data.amount).equal(20);
+      expect(data.status).equal(1);
+      expect(data.wallet).equal('192');
+    });
   });
 });
--- test/banking/withdrawal/withdrawalHistory.js
+++ test/banking/withdrawal/withdrawalHistory.js
@@ -1,28 +1,78 @@
 import { expect } from 'chai';
 import { register } from '../../../src/methods/register';
-import { randomStr } from '../../../src/randomizer';
-import { mysqlConnection } from '../../../src/methods/mysqlConnection';
 import { banking } from '../../../src/methods/banking';
+import { sleep } from '../../../src/methods/utils';
+import { mail } from '../../../src/methods/mail';
+import { userPool } from '../../../src/methods/userPool';
+import { logOut } from '../../../src/methods/user';
+import { userList } from '../../../src/methods/userList';
 
 // returns withdrawals sorted by time
 describe('Withdrawal history', () => {
-  it('C19359 - (+) without withdrawal', async () => {
-    await register.oneClickReg();
-    const { data } = await socket.send('BANKING:withdrawal-history');
-    // console.log(data);
-    expect(data.length).equal(0);
+  const USERS_NUMBER = 2;
+  const BALANCE = 120;
+  let receivedMail = {};
+  let currentUser = {};
+  let users = [];
+
+  beforeAll(async () => {
+    // формируем пул юзеров
+    users = await userPool.usersWithBalanceRubAndConfirmCodes(USERS_NUMBER, BALANCE);
   });
 
-  it('C19360 -(+) with withdrawal @dev', async () => {
-    const user = await register.usualReg({
-      email: `${randomStr(5)}_test@mail.ru`,
+  describe('User without money', () => {
+    it('C19359 - (+) without withdrawal', async () => {
+      await register.oneClickReg();
+      const { data } = await socket.send('BANKING:withdrawal-history');
+      // console.log(data);
+      expect(data.length).equal(0);
     });
-    await mysqlConnection.executeQuery(`UPDATE 1win.ma_balance SET amount = 200 WHERE id_user = ${user.data.id} ;`);
-    await banking.withdrawalCreate(100, '1111222233334444', 'card_rub', 'RUB');
-    const { data } = await socket.send('BANKING:withdrawal-history');
-    // console.log(data);
-    expect(data[0].payment_system).equal('card_rub');
-    expect(data[0].amount).equal(100);
-    expect(data[0].status).equal(0);
+  });
+
+  describe('User with money', () => {
+    beforeEach(async () => {
+      await logOut();
+      currentUser = users.pop();
+      await userList.loginWithParams(currentUser.email, currentUser.password);
+    });
+
+    it('C19360 -(+) with withdrawal @dev', async () => {
+      await banking.withdrawalCreate(100, '1111222233334444', 'card_rub', 'RUB');
+      // задержка для получения письма
+      await sleep(4000);
+      receivedMail = await mail.getMessage(currentUser.email);
+      const confirm = await socket.send('BANKING:withdrawal-confirm', { code: receivedMail.code });
+
+      const { data } = await socket.send('BANKING:withdrawal-history');
+      // console.log(data);
+      expect(data[0].payment_system).equal('card_rub');
+      expect(data[0].amount).equal(100);
+      expect(data[0].status).equal(0);
+    });
+
+
+    it('C396397 (+) - Sort list, first must be last withdrawal ', async () => {
+      await banking.transferCreate(20, 'RUB');
+      await sleep(4000);
+      receivedMail = await mail.getMessage(currentUser.email);
+      await socket.send('BANKING:transfer-confirm', { code: receivedMail.code });
+
+      await banking.withdrawalCreate(100, '5469550073662048', 'card_rub', 'RUB');
+
+      await sleep(4000);
+      receivedMail = await mail.getMessage(currentUser.email);
+      const confirm = await socket.send('BANKING:withdrawal-confirm', { code: receivedMail.code });
+
+      const { data } = await socket.send('BANKING:withdrawal-history');
+
+      // console.log(data);
+
+      expect(data[0].payment_system).equal('card_rub');
+      expect(data[0].amount).equal(100);
+      expect(data[0].status).equal(0);
+      expect(data[1].payment_system).equal('money-transfer');
+      expect(data[1].amount).equal(20);
+      expect(data[1].status).equal(1);
+    });
   });
 });
