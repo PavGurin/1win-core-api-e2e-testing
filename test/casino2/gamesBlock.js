952f72c1fecbf4263b6583feeb21dbf8eb1f0826
Lina <lina.qa88@gmail.com>
2019-08-30 18:13:32 +0300

    fix request

--- test/casino2/gamesBlock.js
+++ test/casino2/gamesBlock.js
@@ -2,13 +2,13 @@ import { expect } from 'chai';
 
 describe('Games-block', () => {
   it('C21139 - (+) valid request', async () => {
-    const { data } = await socket.send('CASINO-2:games-block', {
+    const { data } = await socket.send('COMMON:casino-widget', {
 
       games: ['spacewars_not_mobile_sw', 'bigbadwolf'],
-      categories: [],
+      categories: [33, 41],
     });
 
-    // console.log(data);
+    console.log(data);
     expect(data['0'].hasDemo).not.equal(null);
   });
 });
