c524603e574550609cdf73ef366389f7264d7e32
Lina <lina.qa88@gmail.com>
2019-09-10 17:21:21 +0300

    fix test for deposit request

--- test/banking/deposit/createRequest/depositCreateRequest.js
+++ test/banking/deposit/createRequest/depositCreateRequest.js
@@ -1,30 +1,26 @@
-import { expect } from 'chai';
 import { register } from '../../../../src/methods/register';
-import { getNewSocket } from '../../../global';
+import { banking } from '../../../../src/methods/banking';
+import { mysqlConnection } from '../../../../src/methods/mysqlConnection';
+import { successDbDeposit } from '../../../../src/expects/exDatabaseTests';
 
-const currency = 'RUB';
 const paymentType = 'card_rub';
+const currency = 'RUB';
+let user = {};
 
 describe('Deposit requests', () => {
-  let socket;
-
   beforeEach(async () => {
-    socket = await getNewSocket();
-    await register.oneClickReg(socket);
+    user = await register.oneClickReg(socket);
   });
 
-  afterEach(() => socket.disconnect());
-
   // TODO больше проверок на PaymentType
-  it.skip('C19376 (-) create without currency', async () => {
-    const { data } = await socket.send('BANKING:deposit-create-request', {
-      amount: 101,
-      wallet: ' ',
-      paymentType,
-      currency,
-    });
-    // console.log(data);
-    expect(data.redirectUrl).not.equal(null);
-    expect(data.message).equal(undefined);
+  it('C19376 (+) valid request', async () => {
+    await banking.depositCreateRequest(101, '3333444455556666',
+      paymentType, currency);
+
+    const dbResult = await mysqlConnection.executeQuery(`SELECT * FROM 1win.ma_deposits
+    WHERE id_user = ${user.data.id} ORDER BY id DESC;`);
+    // console.log(dbResult);
+    successDbDeposit(dbResult, 101, '3333444455556666',
+      'card_rub', 'RUB');
   });
 });
