4ae3de4652e22a6f43e959cfed55427f9186c244
pavgurin <kalliak1993@yandex.ru>
2019-09-24 17:56:46 +0300

    test

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@ export default {
 // https://1winufeg.xyz/
 // https://master_staging.staging.1win-prodlike.tech
 // https://master.staging.1win-prodlike.tech/
-// 'https://virtual-sport.staging.1win-prodlike.tech/'
+// 'https://virtual-sport.staging.1win-prodlike.tech/'1
