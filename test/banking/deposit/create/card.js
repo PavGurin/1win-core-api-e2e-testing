7c9f6b8f81f11de4798502075792740e0863b6cc
Lina <lina.qa88@gmail.com>
2019-10-15 00:03:14 +0300

    test

---
+++ test/banking/deposit/create/card.js
@@ -0,0 +1,125 @@
+import { banking } from '../../../../src/methods/banking';
+import { successDepositCreate } from '../../../../src/expects/exBanking';
+import { checkErrMsg } from '../../../../src/responseChecker';
+import { getNewSocket } from '../../../global';
+import { register } from '../../../../src/methods/register';
+
+const paymentType = 'card';
+const currency = 'USD';
+
+describe.skip('Create deposite for card_rub - USD @master', () => {
+  beforeEach(async () => {
+    socket = await getNewSocket();
+    await register.oneClickRegUSD(socket);
+  });
+
+  afterEach(() => socket.disconnect());
+
+
+  it(' (+) amount = 100 & wallet = empty', async () => {
+    const { data } = await banking.depositCreate(
+      100, '', paymentType, currency,
+    );
+    // console.log(data);
+    successDepositCreate(data, currency,
+      paymentType, 100);
+  });
+
+  it(' (+) amount = 100.01 & wallet = symbols', async () => {
+    const { data } = await banking.depositCreate(
+      100.01, '123 autotests', paymentType, currency,
+    );
+    // console.log(data);
+    successDepositCreate(data, currency,
+      paymentType, 100.01);
+  });
+
+  it(' amount = 2000 & wallet = symbols', async () => {
+    const { data } = await banking.depositCreate(
+      2000, 'порпорпорпэ', paymentType, currency,
+    );
+    // console.log(data);
+    successDepositCreate(data, currency,
+      paymentType, 2000);
+  });
+
+  it(' min amount & wallet = symbols', async () => {
+    const { data } = await banking.depositCreate(10, '123234345456 etryrt', paymentType, currency);
+    // console.log(data);
+    successDepositCreate(data, currency,
+      paymentType, 10);
+  });
+
+  it('> min amount & wallet = symbols', async () => {
+    const { data } = await banking.depositCreate(11, '12№%:№%:45456etryrt', paymentType, currency);
+    // console.log(data);
+    successDepositCreate(data, currency,
+      paymentType, 11);
+  });
+
+  it(' max amount & wallet = numbers', async () => {
+    const { data } = await banking.depositCreate(100000, '09090909999',
+      paymentType, currency);
+    // console.log(data);
+    successDepositCreate(data, currency,
+      paymentType, 100000);
+  });
+
+  it('< max amount & wallet = numbers', async () => {
+    const { data } = await banking.depositCreate(99999, '0[[[?<><?999',
+      paymentType, currency);
+    // console.log(data);
+    successDepositCreate(data, currency,
+      paymentType, 99999);
+  });
+
+  // Не знаю, какой должен быть результат
+  it(' wallet = undefined', async () => {
+    const { data } = await banking.depositCreate(100, undefined,
+      paymentType, currency);
+    // console.log(data);
+    successDepositCreate(data, currency,
+      paymentType, 100);
+  });
+
+  it(' without currency', async () => {
+    const { data } = await socket.send('BANKING:deposit-create', {
+      amount: '100',
+      wallet: '00001111222223333',
+      paymentType,
+    });
+    // console.log(data);
+    successDepositCreate(data, currency,
+      paymentType, 100);
+  });
+});
+
+describe.skip('Create deposite for card_rub invalid - USD', () => {
+  it(' amount < min amount', async () => {
+    const { data } = await banking.depositCreate(0.6, '',
+      paymentType, currency);
+    // console.log(data);
+    checkErrMsg(data, 400, 'Неверная сумма');
+  });
+
+  it(' 1 < amount < min amount', async () => {
+    const { data } = await banking.depositCreate(9, '',
+      paymentType, currency);
+    // console.log(data);
+    checkErrMsg(data, 400, 'Неверная сумма');
+  });
+
+  it(' amount > max amount', async () => {
+    const { data } = await banking.depositCreate(100001, '',
+      paymentType, currency);
+    // console.log(data);
+    checkErrMsg(data, 400, 'Неверная сумма');
+  });
+
+  it(' amount doudle > max amount ', async () => {
+    const { data } = await banking.depositCreate(100000.56, '',
+      paymentType, currency);
+    // console.log(data);
+    checkErrMsg(data, 400, 'Неверная сумма');
+  });
+});
