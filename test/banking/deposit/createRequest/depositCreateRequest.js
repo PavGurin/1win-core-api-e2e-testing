df7bfde7b53143a65f0e93ed20bc20708ad73d91
Lina <lina.qa88@gmail.com>
2019-10-15 14:52:13 +0300

    delete incorrect tests for deposit request usd/eur

--- test/banking/deposit/createRequest/depositCreateRequest.js
+++ test/banking/deposit/createRequest/depositCreateRequest.js
@@ -16,9 +16,8 @@ describe('Deposit requests', () => {
       user = await register.oneClickReg(socket);
     });
 
-    it('C19376 (+) valid request', async () => {
+    it('C19376 - (+) valid request', async () => {
       await banking.depositCreateRequest(socket, '3333444455556666', paymentType, currency, 101);
-
       const dbResult = await mysqlConnection.executeQuery(`SELECT * FROM 1win.ma_deposits
     WHERE id_user = ${user.data.id} ORDER BY id DESC;`);
       // console.log(dbResult);
@@ -36,107 +35,14 @@ describe('Deposit requests', () => {
       // console.log(user);
     });
 
-    it(' (+) valid request, \'card_rub\'', async () => {
-      await banking.depositCreateRequest(socket, '3333444455556666', 'card_rub', currency, 101);
+    it('C668350 - (+) valid request, \'card\'', async () => {
+      await banking.depositCreateRequest(socket, '3333444455556666', 'card', currency, 101);
 
       const dbResult = await mysqlConnection.executeQuery(`SELECT * FROM 1win.ma_deposits
     WHERE id_user = ${user.data.id} ORDER BY id DESC;`);
       // console.log(dbResult);
       successDbDeposit(dbResult, 101, '3333444455556666',
-        'card_rub', 'USD');
-    });
-
-    it(' (+) valid request, \'beeline_rub\'', async () => {
-      await banking.depositCreateRequest(socket, '+79001234567', 'beeline_rub', currency, 101);
-
-      const dbResult = await mysqlConnection.executeQuery(`SELECT * FROM 1win.ma_deposits
-    WHERE id_user = ${user.data.id} ORDER BY id DESC;`);
-      // console.log(dbResult);
-      successDbDeposit(dbResult, 101, '9001234567',
-        'beeline_rub', 'USD');
-    });
-
-    it(' (+) valid request, \'btc_usd\'', async () => {
-      await banking.depositCreateRequest(socket, '+79001234567', 'btc_usd', currency, 10100);
-      // console.log(ban);
-
-      const dbResult = await mysqlConnection.executeQuery(`SELECT * FROM 1win.ma_deposits
-    WHERE id_user = ${user.data.id} ORDER BY id DESC;`);
-      // console.log(dbResult);
-      successDbDeposit(dbResult, 10100, '+79001234567',
-        'btc_usd', 'USD');
-    });
-    it(' (+) valid request, \'eth_usd\'', async () => {
-      await banking.depositCreateRequest(socket, '+79001234567', 'eth_usd', currency, 10100);
-      // console.log(ban);
-
-      const dbResult = await mysqlConnection.executeQuery(`SELECT * FROM 1win.ma_deposits
-    WHERE id_user = ${user.data.id} ORDER BY id DESC;`);
-      // console.log(dbResult);
-      successDbDeposit(dbResult, 10100, '+79001234567',
-        'eth_usd', 'USD');
-    });
-
-    it(' (+) valid request, \'megafon_rub\'', async () => {
-      await banking.depositCreateRequest(socket, '+79001234567', 'megafon_rub', currency, 101);
-      // console.log(ban);
-
-      const dbResult = await mysqlConnection.executeQuery(`SELECT * FROM 1win.ma_deposits
-    WHERE id_user = ${user.data.id} ORDER BY id DESC;`);
-      // console.log(dbResult);
-      successDbDeposit(dbResult, 101, '9001234567',
-        'megafon_rub', 'USD');
-    });
-
-    it(' (+) valid request, \'mts_rub\'', async () => {
-      await banking.depositCreateRequest(socket, '+79001234567', 'mts_rub', currency, 101);
-      // console.log(ban);
-
-      const dbResult = await mysqlConnection.executeQuery(`SELECT * FROM 1win.ma_deposits
-    WHERE id_user = ${user.data.id} ORDER BY id DESC;`);
-      // console.log(dbResult);
-      successDbDeposit(dbResult, 101, '9001234567',
-        'mts_rub', 'USD');
-    });
-
-    it(' (+) valid request, \'piastrix_rub\'', async () => {
-      await banking.depositCreateRequest(socket, '+79001234567', 'piastrix_rub', currency, 101);
-
-      const dbResult = await mysqlConnection.executeQuery(`SELECT * FROM 1win.ma_deposits
-    WHERE id_user = ${user.data.id} ORDER BY id DESC;`);
-      // console.log(dbResult);
-      successDbDeposit(dbResult, 101, '+79001234567',
-        'piastrix_rub', 'USD');
-    });
-
-    it(' (+) valid request, \'qiwi_rub\'', async () => {
-      await banking.depositCreateRequest(socket, '+79001234567', 'qiwi_rub', currency, 101);
-
-      const dbResult = await mysqlConnection.executeQuery(`SELECT * FROM 1win.ma_deposits
-    WHERE id_user = ${user.data.id} ORDER BY id DESC;`);
-      // console.log(dbResult);
-      successDbDeposit(dbResult, 101, '79001234567',
-        'qiwi_rub', 'USD');
-    });
-
-    it(' (+) valid request, \'tele2_rub\'', async () => {
-      await banking.depositCreateRequest(socket, '9001234567', 'tele2_rub', currency, 101);
-
-      const dbResult = await mysqlConnection.executeQuery(`SELECT * FROM 1win.ma_deposits
-    WHERE id_user = ${user.data.id} ORDER BY id DESC;`);
-      // console.log(dbResult);
-      successDbDeposit(dbResult, 101, '9001234567',
-        'tele2_rub', 'USD');
-    });
-
-    it(' (+) valid request, \'yamoney_rub\'', async () => {
-      await banking.depositCreateRequest(socket, '9001234567', 'yamoney_rub', currency, 101);
-
-      const dbResult = await mysqlConnection.executeQuery(`SELECT * FROM 1win.ma_deposits
-    WHERE id_user = ${user.data.id} ORDER BY id DESC;`);
-      // console.log(dbResult);
-      successDbDeposit(dbResult, 101, '9001234567',
-        'yamoney_rub', 'USD');
+        'card', 'USD');
     });
   });
 });
