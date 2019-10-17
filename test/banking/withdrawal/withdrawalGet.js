6fff4794adc19506684a35a0aca9038966e9d528
Lina <lina.qa88@gmail.com>
2019-10-17 19:52:12 +0300

    fix test

--- test/banking/withdrawal/withdrawalGet.js
+++ test/banking/withdrawal/withdrawalGet.js
@@ -10,7 +10,7 @@ import { getNewSocket } from '../../global';
 
 describe('Withdrawal get', () => {
   const USERS_NUMBER = 2;
-  const BALANCE = 120;
+  const BALANCE = 220;
   let receivedMail = {};
   let currentUser = {};
   let users = [];
@@ -20,20 +20,21 @@ describe('Withdrawal get', () => {
     socket = await getNewSocket();
     // формируем пул юзеров
     users = await userPool.usersWithBalanceRubAndConfirmCodes(socket, USERS_NUMBER, BALANCE);
-    await socket.disconnect();
   });
 
-  beforeEach(async () => {
-    socket = await getNewSocket();
-  });
-
-  afterEach(async () => { await socket.disconnect(); });
+  afterAll(async () => { await socket.disconnect(); });
 
   describe('Invalid Id withdrawal', () => {
     it('C19364 (-) Get - Bad request, id is required ', async () => {
       const { data } = await socket.send('BANKING:withdrawal-get', { id: null });
-      // console.log(data);
+       // console.log(data);
       checkErrMsg(data, 400, 'Bad request, id is required');
+    });
+
+    it('C19361 (-) Get - withdrawal not found', async () => {
+      const { data } = await socket.send('BANKING:withdrawal-get', { id: 0 });
+       // console.log(data);
+      checkErrMsg(data, 404, 'Выплата не найдена');
     });
   });
 
@@ -42,12 +43,6 @@ describe('Withdrawal get', () => {
       await logOut();
       currentUser = users.pop();
       await userList.loginWithParams(socket, currentUser.email, currentUser.password);
-    });
-
-    it('C19361 (-) Get - withdrawal not found - auth', async () => {
-      const { data } = await socket.send('BANKING:withdrawal-get', { id: 0 });
-      console.log(data);
-      checkErrMsg(data, 404, 'Выплата не найдена');
     });
 
     it('C19363 (+) Get - 100 RUB card_rub ', async () => {
