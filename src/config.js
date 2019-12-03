633d4ae41180f01d7b31b377c07cb233992d5f5b
pavgurin <kalliak1993@yandex.ru>
2019-12-03 15:24:49 +0300

    updated cases-rating test according to https://fbet-gitlab.ex2b.co/backend/money-cases/issues/21 changed chai check to jestExtended

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  backendURL: process.env.TEST_URL || 'https://1win.dev/',
+  backendURL: process.env.TEST_URL || 'https://master_staging.staging.1win-prodlike.tech/',
 };
 // https://1winufeg.xyz/
 // https://1win.dev/
